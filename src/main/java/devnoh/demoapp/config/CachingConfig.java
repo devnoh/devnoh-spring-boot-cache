a6578c93567b04fe8eba0b5685af44e178f23582
Sehwan Noh <devnoh@gmail.com>
2016-11-03 14:09:06 -0700

    .

--- src/main/java/devnoh/demoapp/config/CachingConfig.java
+++ src/main/java/devnoh/demoapp/config/CachingConfig.java
@@ -3,6 +3,7 @@ package devnoh.demoapp.config;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +16,10 @@ import java.util.Arrays;
 @Configuration
 @EnableCaching
 public class CachingConfig {
-
     /*
     @Bean
     public CacheManager cacheManager() {
+        //ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
         SimpleCacheManager cacheManager = new SimpleCacheManager();
         cacheManager.setCaches(Arrays.asList(
                 new ConcurrentMapCache("books"),
